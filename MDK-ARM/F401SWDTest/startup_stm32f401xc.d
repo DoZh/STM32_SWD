b68c38942a90a5b9b86b5070aec57771e7c41aa6
MAGI <TATQAQTAT@gmail.com>
2017-09-26 18:13:19 +0800

    Init project with git, now can read ID code use SWD port simulation by SPI 8bit MSB mode.

---
+++ MDK-ARM/F401SWDTest/startup_stm32f401xc.d
@@ -0,0 +1,1 @@
+f401swdtest\startup_stm32f401xc.o: startup_stm32f401xc.s
---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'F401SWDTest' 
+ * Target:  'F401SWDTest' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
